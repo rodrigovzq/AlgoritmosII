51bf286b5e82810cd593d448a9af3cbe56a5144c
Rodrigo Vazquez <rvazquez@fi.uba.ar>
2021-05-08 16:53:43 -0300

    Comienzo tp cola

---
+++ TP-indiv/cola/cola.c
@@ -0,0 +1,10 @@
+#include "cola.h"
+/*******************************************************************
+ *               		     TP Cola                               *
+ *		 Facultad de Ingenieria de la Universidad de Buenos Aires  *
+ *												Vazquez, Rodrigo   *
+ ******************************************************************/
+
+#include <stdlib.h>
+
+// cambios
