6940bbbe3faf7528cda4bb81cfb3985c35da28d3
Rodrigo Vazquez <rvazquez@fi.uba.ar>
2021-05-08 21:29:22 -0300

    Alan termino su trabajo

--- TP-indiv/cola/cola.c
+++ TP-indiv/cola/cola.c
@@ -19,43 +19,69 @@ struct cola
     nodo_t *ultimo;
 };
 
-// Crea una cola.
-// Post: devuelve una nueva cola vacía.
-cola_t *cola_crear(void)
+cola_t *cola_crear(void) //LISTO
 {
     cola_t *cola = malloc(sizeof(cola_t));
     if (cola == NULL)
         return NULL;
-    pila->primero = NULL;
-    pila->ultimo = NULL;
+    cola->primero = NULL;
+    cola->ultimo = NULL;
     return cola;
 }
-// Destruye la cola. Si se recibe la función destruir_dato por parámetro,
-// para cada uno de los elementos de la cola llama a destruir_dato.
-// Pre: la cola fue creada. destruir_dato es una función capaz de destruir
-// los datos de la cola, o NULL en caso de que no se la utilice.
-// Post: se eliminaron todos los elementos de la cola.
-void cola_destruir(cola_t *cola, void (*destruir_dato)(void *));
 
-// Devuelve verdadero si la cola no tiene elementos encolados, false en caso contrario.
-// Pre: la cola fue creada.
-bool cola_esta_vacia(const cola_t *cola);
+void cola_destruir(cola_t *cola, void (*destruir_dato)(void *))
+{
+    if (cola_esta_vacia(cola))
+        return;
+    while (cola->primero)
+    {
+        void *dato = cola_desencolar(cola);
+        if (*destruir_dato)
+        {
+            (*destruir_dato)(dato);
+        }
+    }
+    free(cola);
+}
 
-// Agrega un nuevo elemento a la cola. Devuelve falso en caso de error.
-// Pre: la cola fue creada.
-// Post: se agregó un nuevo elemento a la cola, valor se encuentra al final
-// de la cola.
-bool cola_encolar(cola_t *cola, void *valor);
+bool cola_esta_vacia(const cola_t *cola) //LISTO
+{
+    return !cola->primero;
+}
 
-// Obtiene el valor del primer elemento de la cola. Si la cola tiene
-// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
-// Pre: la cola fue creada.
-// Post: se devolvió el primer elemento de la cola, cuando no está vacía.
-void *cola_ver_primero(const cola_t *cola);
+bool cola_encolar(cola_t *cola, void *valor) //LISTO
+{
+    nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
+    if (nuevo_nodo == NULL)
+        return false;
+    nuevo_nodo->sig = NULL;
+    nuevo_nodo->dato = valor;
+    if (cola_esta_vacia(cola))
+    {
+        cola->primero = nuevo_nodo;
+    }
+    else
+    {
+        cola->ultimo->sig = nuevo_nodo;
+    }
+    cola->ultimo = nuevo_nodo;
+    return true;
+}
 
-// Saca el primer elemento de la cola. Si la cola tiene elementos, se quita el
-// primero de la cola, y se devuelve su valor, si está vacía, devuelve NULL.
-// Pre: la cola fue creada.
-// Post: se devolvió el valor del primer elemento anterior, la cola
-// contiene un elemento menos, si la cola no estaba vacía.
-void *cola_desencolar(cola_t *cola);
+void *cola_ver_primero(const cola_t *cola) //LISTO
+{
+    if (cola_esta_vacia(cola))
+        return NULL;
+    return cola->primero->dato;
+}
+
+void *cola_desencolar(cola_t *cola)
+{
+    if (cola_esta_vacia(cola))
+        return NULL;
+    void *dato = cola->primero->dato;           //rescata dato a devolver
+    nodo_t *nuevo_primero = cola->primero->sig; //en caso de que haya un elemento, asignara NULL;
+    free(cola->primero);
+    cola->primero = nuevo_primero;
+    return dato;
+}
---
+++ TP-indiv/cola/pruebas_cola.c
@@ -0,0 +1,18 @@
+#include "cola.h"
+#include "testing.h"
+#include <stdio.h>
+
+#ifndef CORRECTOR
+void pruebas_cola_estudiante(void)
+{
+    // TODO: HACER PRUEBAS
+    return;
+}
+
+int main(void)
+{
+    pruebas_cola_estudiante();
+    return failure_count() > 0; // Indica si falló alguna prueba.
+}
+
+#endif
